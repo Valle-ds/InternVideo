7462f75d0a2bcee59083a041bba61445b143c363
Valle-ds <83124979+Valle-ds@users.noreply.github.com>
2023-02-19 21:05:10 +0300

    oooouuu

--- Downstream/multi-modalities-downstream/meta_data/download_files.sh
+++ Downstream/multi-modalities-downstream/meta_data/download_files.sh
@@ -1,1 +1,2 @@
 wget https://pjlab-gvm-data.oss-cn-shanghai.aliyuncs.com/internvideo/pretrain/InternVideo-MM-B-16.ckpt
+wget https://www.robots.ox.ac.uk/~maxbain/frozen-in-time/data/MSRVTT.zip -P meta_data; unzip -qq meta_data/msrvtt/MSRVTT.zip
